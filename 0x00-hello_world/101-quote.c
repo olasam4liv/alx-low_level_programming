0733ff441faa2b0d65d79e44a4c26f51df12fb58
Samuel Olatunji <samuel.olatunji@coronationinsurance.com.ng>
2022-06-30 22:00:18 +0100

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: 1 on success
+ */
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (1);
+}
