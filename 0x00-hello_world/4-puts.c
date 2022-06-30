d3367322c0c2ba612c6fd93c2a5c0a1c6872fcf5
Samuel Olatunji <samuel.olatunji@coronationinsurance.com.ng>
2022-06-30 21:43:08 +0100

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ *
+ * Return: Always 0 on success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
