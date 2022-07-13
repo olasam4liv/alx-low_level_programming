12764e36e1f97b5b3826f20db9cb3ff21f0cfc18
Samuel Olatunji <samuel.olatunji@coronationinsurance.com.ng>
2022-07-13 23:16:58 +0100

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _atoi - changes a string to an integer
+ * @s: the string to be changed
+ * Return: converted string to integer
+ */
+int _atoi(char *s)
+{
+	int i = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			i *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = num * 10 + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * i);
+}
