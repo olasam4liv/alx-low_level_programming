05335e1fa48f79a2326773962e3d31097d2b57ed
Samuel Olatunji <samuel.olatunji@coronationinsurance.com.ng>
2022-07-15 12:53:06 +0100

    function to encode a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * leet - function that encodes a string into 1337
- * @s: string array
- * Return: string array
+ * leet - a function that encodes a string into 1337
+ * @d: string
+ * Return: dest
  */
-
-char *leet(char *s)
+char *leet(char *d)
 {
-	int i;
+	int count = 0, i;
+	int low_case[] = {97, 101, 111, 116, 108};
+	int upper_case[] = {65, 69, 79, 84, 76};
+	int num[] = {52, 51, 48, 55, 49};
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*(d + count) != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (i = 0; i < 5; i++)
+		{
+			if (*(d + count) == low_case[i] || *(d + count) == upper_case[i])
+			{
+				*(d + count) = num[i];
+				break;
+			}
+		}
+		count++;
 	}
-
-	return (s);
+	return (d);
 }
