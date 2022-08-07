64c4d49b89b26adfba4079f8ecf17378d13a4110
Samuel Olatunji <samuel.olatunji@coronationinsurance.com.ng>
2022-08-07 20:13:33 +0100

    HEADER

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,20 +1,18 @@
-#include <stdarg.h>
-#include <stdio.h>
-/**
- * struct args_t - contain the type and the function
- * @format: the format to print
- * @function: the functions that prints
- * Description: contains the type and the format to print.
- */
-typedef struct args_t
-{
-	char *format;
-	void (*function)();
-} ars_t;
+#ifndef VARIADIC_FNS
+#define VARIADIC_FNS
 
+#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+
+typedef struct validTypes
+{
+	char *valid;
+	void (*f)();
+} v_types;
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif /* VARIAFUNC */
+#endif
